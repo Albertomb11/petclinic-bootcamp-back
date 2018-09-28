98211a15b1a57ca89c80db1042d445b88ccdc605
Alberto <alberto1sevilla@hotmail.com>
2018-09-28 11:14:58 +0200

    subir servicio

--- src/main/java/com/bootcamp/petclinic/service/impl/OfertaServiceImpl.java
+++ src/main/java/com/bootcamp/petclinic/service/impl/OfertaServiceImpl.java
@@ -2,16 +2,43 @@ package com.bootcamp.petclinic.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.bootcamp.petclinic.entity.Oferta;
 import com.bootcamp.petclinic.service.OfertaService;
 
 @Service
 public class OfertaServiceImpl implements OfertaService{ 
 	
-	@Autowired
 	private OfertaRepository ofertaRepo;
-
-	public OfertaServiceImpl() {
-		// TODO Auto-generated constructor stub
+	
+	@Autowired
+	public OfertaServiceImpl(OfertaRepository ofertaRepo) {
+		this.ofertaRepo = ofertaRepo;
+	}
+	
+	public Oferta findById(int id) {
+		return this.ofertaRepo.findById(id);
+	}
+	
+	public Oferta findAll() {
+		return this.ofertaRepo.findAll();
+	}
+	
+	public void saveOferta(Oferta oferta) {
+		return this.ofertaRepo.save(oferta);
+	}
+	
+	public void insertOferta(Oferta oferta) {
+		return this.ofertaRepo.insert(oferta);
+	}
+	
+	public void updateOferta(Oferta oferta) {
+		return this.ofertaRepo.update(oferta);
+	}
+	
+	public void deleteOferta(Oferta oferta) {
+		return this.ofertaRepo.delete(oferta);
 	}
 
+
+	
 }
